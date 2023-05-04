9780ab3b1feee17b48b70aa078c4da704feb36d2
Lucas Chaves <y3tsec@protonmail.com>
2023-05-04 19:46:42 -0300

    Made some changes to the input string. Now quote marks are no longer needed

--- main.cpp
+++ main.cpp
@@ -7,13 +7,16 @@ int main(int argc, char **argv){
     string stringToSay;
     for(int i = 1; i < argc; i++){
         stringToSay.insert(stringToSay.length(), argv[i]);
+        if(i < (argc - 1)) {
+            stringToSay.insert(stringToSay.length(), " ");
+        }
     }
 
     string rinoAscii = " |     ,       ,\n |    /|    |\\./'.\n |   | |  ,  \\|| ,|\n \\   \\  \\_(\\.-\"\"\\//.  \n  .-'`\"\"``\"` _   ` `-.`\"\"\"--.._      _..----. __\n  | '~`      o\\                `\"---\"        `. `\"-.==,\n   \\,.-;    `\"`                                |`\"\"`===`\n     (`            /                           |\n      `-----.____.;          \\     |           ;\n                  \\__         |    \\          /\n                 .'         .'      \\        ' `,\n                /          /         '._        |\n                |    '.---;`-.____.-'`\\ `\"\"`;   |\n                |     _\\   \\    '.     )   /    \\ \n                \\-,--( /   /    _/   .'   |_ _ .-)\n                 '----;)__;    (`.-. ;    `-:.;-'\n                                `\"\"\"\"`\n";
 
     if(stringToSay.empty()){
-        cout << "Write the wanted string inside quote marks like this:" <<  endl;
-        cout << "rinosay \"rinosay is awesome!\" " <<  endl;
+        cout << "Write the wanted string like this:" <<  endl;
+        cout << "rinosay rinosay is awesome! " <<  endl;
     } else if (stringToSay.length() > 82) {
         cout << "String too big, Max char number is 82" <<  endl;
     }else {
